a8db64e91a5a05fcc3aea6dc1acd13cfaf2c5ff4
2711733722 <2711733722@qq.com>
2019-09-03 09:50:48 +0800

    jQuery.js提交信息

---
+++ WebContent/js/javascript.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+	
+	$(".btn").bind("click",function(){
+		window.location.href='main_index';
+	});
+	
+	
+	$(".return").bind("click",function(){
+		window.location.href='admin_mana';
+	});
+	
+
+
+});
+
+
+
+
+
+
